faa7d93b06e7a0c3b2c5522c461f27e780f02201
Tristan Straub <tstraub@seek.com.au>
2016-12-30 11:55:40 +1100

    rename tasks

--- src/clj_aws_static_website/boot.clj
+++ src/clj_aws_static_website/boot.clj
@@ -3,7 +3,7 @@
             [clj-aws-static-website.stack :as stack]
             [clj-aws-static-website.contents :as contents]))
 
-(boot/deftask create
+(boot/deftask create-stack
   "Create aws stack for s3/cloudfront website"
   [s stack   STR str "Name of stack"
    d domain STR str "Create stack for domain"]
@@ -11,7 +11,7 @@
     (println (stack/create! stack domain))
     fileset))
 
-(boot/deftask push
+(boot/deftask push-contents
   "Push website contents to s3"
   [d domain STR str "Create stack for domain"
    p path   STR str "Path of files to deploy"]
